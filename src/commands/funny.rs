56a877efe4773a5370a4c3bd43bb0e6db800baec
fabseman <fabse_utilities@pm.me>
2024-07-02 10:09:32 +0200

    Initial rework of command, now searches for webhooks

--- src/commands/funny.rs
+++ src/commands/funny.rs
@@ -1,7 +1,8 @@
 use crate::types::{Context, Error};
 
-use poise::serenity_prelude::{self as serenity, CreateMessage};
+use poise::serenity_prelude::{self as serenity, CreateMessage, ExecuteWebhook};
 use poise::CreateReply;
+use serenity::json::json;
 
 /// Send an anonymous message
 #[poise::command(slash_command, prefix_command)]
@@ -48,12 +49,59 @@ pub async fn user_misuse(
     #[description = "Target"] user: poise::serenity_prelude::User,
     #[description = "Message to send"]
     #[rest]
-    _message: String,
+    message: String,
 ) -> Result<(), Error> {
-    let _avatar_url = user.avatar_url();
-    let _name = user.name;
-    let _channel_id = ctx.channel_id();
-    ctx.send(CreateReply::default().content("thou can't be trusted with this power"))
-        .await?;
+    let avatar_url = user.avatar_url().unwrap();
+    let name = user.name;
+    let channel_id = ctx.channel_id();
+    let webhook_info = json!({
+        "name": name,
+        "avatar": avatar_url
+    });
+    let existing_webhooks = match channel_id.webhooks(&ctx.http()).await {
+        Ok(webhooks) => webhooks,
+        Err(err) => {
+            eprintln!("Error retrieving webhooks: {:?}", err);
+            Vec::new()
+        }
+    };
+    if existing_webhooks.len() >= 15 {
+        for webhook in &existing_webhooks {
+            let _ = (ctx.http()).delete_webhook(webhook.id, None).await;
+        }
+    }
+    if let Some(existing_webhook) = existing_webhooks
+        .iter()
+        .find(|webhook| webhook.name.as_deref() == Some("fabsemanbots"))
+    {
+        let _ = existing_webhook
+            .execute(
+                &ctx.http(),
+                false,
+                ExecuteWebhook::new()
+                    .username(name)
+                    .avatar_url(avatar_url)
+                    .content(message),
+            )
+            .await;
+    } else {
+        let new_webhook = ctx
+            .http()
+            .create_webhook(channel_id, &webhook_info, None)
+            .await;
+        let _ = new_webhook
+            .unwrap()
+            .execute(
+                &ctx.http(),
+                false,
+                ExecuteWebhook::new()
+                    .username(name)
+                    .avatar_url(avatar_url)
+                    .content(message),
+            )
+            .await;
+    }
+    // ctx.send(CreateReply::default().content("thou can't be trusted with this power"))
+    //   .await?;
     Ok(())
 }
