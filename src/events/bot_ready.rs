55b44ddfb2dfc1b4c82ef29c1c2981b612c7c6e8
fabseman <fabse_utilities@pm.me>
2024-10-30 11:02:01 +0900

    added count of user-installs + initial TOS and PP (WIP)

--- src/events/bot_ready.rs
+++ src/events/bot_ready.rs
@@ -12,8 +12,12 @@ pub async fn handle_ready(
     data_about_bot: &Ready,
     framework_context: FrameworkContext<'_, Data, Error>,
 ) -> Result<(), Error> {
+    let user_count = match ctx.http.get_current_application_info().await {
+        Ok(info) => info.approximate_user_install_count.unwrap_or(0),
+        Err(_) => 0,
+    };
     info!(
-        "Logged in as {} in {} servers",
+        "Logged in as {} in {} server(s) and installed for {user_count} user(s)",
         data_about_bot.user.name,
         data_about_bot.guilds.len(),
     );
